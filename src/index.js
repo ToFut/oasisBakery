5251b64cba62402f105efc043c687dd2a52ec972
Segev <segev@futurix.com>
2025-07-25 16:06:17 -0400

    Fix BakeryOSApp import in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BakeryOSApp from './BakeryOSApp';
+import BakeryOSApp from './components/BakeryOSApp.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
